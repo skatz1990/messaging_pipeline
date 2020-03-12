8020437d903306744aa43442f097c4c2771bd224
Shahar Katz <skatz@blizzard.com>
2020-03-11 20:54:52 -0700

    Added Cassandra k8s deployment

---
+++ .integration/setup-cassandra.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+
+POD=$(k get pod -l app=cassandra -o jsonpath="{.items[0].metadata.name}")
+k exec -it $POD bash
+cqlsh
+CREATE KEYSPACE kafka WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};
+CREATE TABLE messages(
+   msg_id int PRIMARY KEY,
+   msg_data text
+   );
+
