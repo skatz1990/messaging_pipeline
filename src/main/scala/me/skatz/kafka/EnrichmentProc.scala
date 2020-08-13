752de23405fce16934b843850c29785612e3edd8
skatz <skatz1990@gmail.com>
2020-08-13 10:05:17 -0400

    Code refactoring

--- src/main/scala/me/skatz/kafka/EnrichmentProc.scala
+++ src/main/scala/me/skatz/kafka/EnrichmentProc.scala
@@ -1,19 +1,12 @@
 package me.skatz.kafka
 
-import java.io.ByteArrayOutputStream
-
 import akka.actor.ActorSystem
 import akka.kafka.scaladsl.{Consumer, Producer}
 import akka.kafka.{CommitterSettings, ProducerMessage, ProducerSettings, Subscriptions}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Keep
-import com.google.gson.Gson
-import com.sksamuel.avro4s.AvroSchema
 import com.typesafe.config.ConfigFactory
-import me.skatz.database.TweeterMessage
-import me.skatz.utils.{Configuration, KafkaUtils}
-import org.apache.avro.generic.{GenericData, GenericDatumWriter, GenericRecord}
-import org.apache.avro.io.{BinaryEncoder, EncoderFactory}
+import me.skatz.utils.{AvroMessageSerializer, Configuration, KafkaUtils}
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.{ByteArraySerializer, StringSerializer}
 import spray.json.DefaultJsonProtocol
@@ -33,7 +26,7 @@ object EnrichmentProc extends App with DefaultJsonProtocol {
   Consumer
     .committableSource(consumerSettings, Subscriptions.topics(Configuration.ingestEnrichTopic))
     .map { msg =>
-      val byteArray = serialize(msg.record.value())
+      val byteArray = AvroMessageSerializer.jsonToGenericByteArray(msg.record.value())
       ProducerMessage.multi(
         List[ProducerRecord[String, Array[Byte]]](
           new ProducerRecord[String, Array[Byte]](Configuration.enrichEsprocTopic, byteArray),
@@ -44,30 +37,4 @@ object EnrichmentProc extends App with DefaultJsonProtocol {
     }
     .toMat(Producer.committableSink(producerSettings, committerSettings))(Keep.both)
     .run()
-
-  def serialize(msg: String): Array[Byte] = {
-    val gson = new Gson
-
-    // Deserialize JSON
-    val message = gson.fromJson(msg, classOf[TweeterMessage])
-
-    val schema = AvroSchema[TweeterMessage]
-    val genericRecord: GenericRecord = new GenericData.Record(schema)
-
-    genericRecord.put("firstName", message.firstName)
-    genericRecord.put("lastName", message.lastName)
-    genericRecord.put("tweet", message.tweet)
-    genericRecord.put("date", message.date)
-
-    // Serialize generic record into byte array
-    val writer = new GenericDatumWriter[GenericRecord](schema)
-    val out = new ByteArrayOutputStream()
-    val encoder: BinaryEncoder = EncoderFactory.get().binaryEncoder(out, null)
-    writer.write(genericRecord, encoder)
-    encoder.flush()
-    out.close()
-    out.close()
-
-    out.toByteArray
-  }
 }
--- src/main/scala/me/skatz/kafka/KafkaProducer.scala
+++ src/main/scala/me/skatz/kafka/KafkaProducer.scala
@@ -2,11 +2,14 @@ package me.skatz.kafka
 
 import akka.Done
 import akka.actor.ActorSystem
+import akka.kafka.ProducerSettings
 import akka.kafka.scaladsl.Producer
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Source
-import me.skatz.utils.{Configuration, JsonHelper, KafkaUtils, MessageGenerator}
+import com.typesafe.config.ConfigFactory
+import me.skatz.utils.{Configuration, JsonHelper, MessageGenerator}
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringSerializer
 
 import scala.concurrent.Future
 
@@ -14,7 +17,9 @@ object KafkaProducer extends App {
   implicit val system: ActorSystem = ActorSystem("Producer")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
 
-  val producerSettings = KafkaUtils.configureProducerSettings()
+  val producerConfig = ConfigFactory.load.getConfig("akka.kafka.producer")
+  val producerSettings = ProducerSettings(producerConfig, new StringSerializer, new StringSerializer)
+    .withBootstrapServers(Configuration.bootstrapServer)
 
   val producerSink: Future[Done] =
     Source(1 to MessageGenerator.numOfTweets)
