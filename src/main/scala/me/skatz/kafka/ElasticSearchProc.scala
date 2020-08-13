8685d0bad8a1479ac0e739eb0846461ec454272a
skatz <skatz1990@gmail.com>
2020-08-12 22:13:30 -0400

    ES fully supports Avro now

--- src/main/scala/me/skatz/kafka/ElasticSearchProc.scala
+++ src/main/scala/me/skatz/kafka/ElasticSearchProc.scala
@@ -1,17 +1,21 @@
 package me.skatz.kafka
 
 import akka.actor.ActorSystem
-import akka.kafka.Subscriptions
 import akka.kafka.javadsl.Consumer
+import akka.kafka.{ConsumerSettings, Subscriptions}
 import akka.stream.ActorMaterializer
 import akka.stream.alpakka.elasticsearch.WriteMessage
 import akka.stream.alpakka.elasticsearch.scaladsl.ElasticsearchSink
 import akka.stream.scaladsl.Flow
-import com.google.gson.Gson
+import com.sksamuel.avro4s.AvroSchema
+import com.typesafe.config.{Config, ConfigFactory}
 import me.skatz.database.TweeterMessage
-import me.skatz.utils.{Configuration, KafkaUtils}
+import me.skatz.utils.Configuration
+import org.apache.avro.generic.{GenericDatumReader, GenericRecord}
+import org.apache.avro.io.DecoderFactory
 import org.apache.http.HttpHost
-import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
+import org.apache.kafka.common.serialization.ByteArrayDeserializer
 import org.elasticsearch.client.RestClient
 import spray.json.{DefaultJsonProtocol, JsonFormat}
 
@@ -21,18 +25,39 @@ object ElasticSearchProc extends App with DefaultJsonProtocol {
   implicit val client: RestClient = RestClient.builder(new HttpHost(Configuration.esUrl, Configuration.esPort.toInt)).build()
   implicit val format: JsonFormat[TweeterMessage] = jsonFormat4(TweeterMessage)
 
-  val consumerSettings = KafkaUtils.configureConsumerSettings()
+  // val consumerSettings = KafkaUtils.configureConsumerSettings()
+  val consumerConfig: Config = ConfigFactory.load.getConfig("akka.kafka.consumer")
+  val consumerSettings = ConsumerSettings(consumerConfig, new ByteArrayDeserializer, new ByteArrayDeserializer)
+    .withBootstrapServers(Configuration.bootstrapServer)
+    .withGroupId(Configuration.groupId)
+    .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
 
   val kafkaSource = Consumer.plainSource(consumerSettings, Subscriptions.topics(Configuration.enrichEsprocTopic))
 
-  val intermediateFlow = Flow[ConsumerRecord[Array[Byte], String]].map { kafkaMessage =>
-    val gson = new Gson
+  val intermediateFlow = Flow[ConsumerRecord[Array[Byte], Array[Byte]]].map { kafkaMessage =>
+    val msg = getMessage(kafkaMessage.value)
 
-    // Deserialize JSON
-    val message = gson.fromJson(kafkaMessage.value(), classOf[TweeterMessage])
-    WriteMessage.createIndexMessage(message)
+    WriteMessage.createIndexMessage(msg.get)
   }
 
   val esSink = ElasticsearchSink.create[TweeterMessage](indexName = "kafka", typeName = "type name")
   kafkaSource.via(intermediateFlow).runWith(esSink, system)
+
+  private def getMessage(message: Array[Byte]) = {
+    val s = (message.map(_.toChar)).mkString
+
+    val schema = AvroSchema[TweeterMessage]
+
+    val reader: GenericDatumReader[GenericRecord] = new GenericDatumReader[GenericRecord](schema)
+    val decoder = DecoderFactory.get().binaryDecoder(message, null)
+    val generic: GenericRecord = reader.read(null, decoder)
+
+    val tweeterMessage = TweeterMessage(
+      generic.get("firstName").toString,
+      generic.get("lastName").toString,
+      generic.get("tweet").toString,
+      generic.get("date").toString)
+
+    Some(tweeterMessage)
+  }
 }
--- src/main/scala/me/skatz/kafka/EnrichmentProc.scala
+++ src/main/scala/me/skatz/kafka/EnrichmentProc.scala
@@ -1,13 +1,21 @@
 package me.skatz.kafka
 
+import java.io.ByteArrayOutputStream
+
 import akka.actor.ActorSystem
 import akka.kafka.scaladsl.{Consumer, Producer}
-import akka.kafka.{CommitterSettings, ProducerMessage, Subscriptions}
+import akka.kafka.{CommitterSettings, ProducerMessage, ProducerSettings, Subscriptions}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Keep
-import com.sksamuel.avro4s.json.JsonToAvroConverter
+import com.google.gson.Gson
+import com.sksamuel.avro4s.AvroSchema
+import com.typesafe.config.ConfigFactory
+import me.skatz.database.TweeterMessage
 import me.skatz.utils.{Configuration, KafkaUtils}
+import org.apache.avro.generic.{GenericData, GenericDatumWriter, GenericRecord}
+import org.apache.avro.io.{BinaryEncoder, EncoderFactory}
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.{ByteArraySerializer, StringSerializer}
 import spray.json.DefaultJsonProtocol
 
 object EnrichmentProc extends App with DefaultJsonProtocol {
@@ -15,16 +23,21 @@ object EnrichmentProc extends App with DefaultJsonProtocol {
   implicit val materializer: ActorMaterializer = ActorMaterializer()
 
   val consumerSettings = KafkaUtils.configureConsumerSettings()
-  val producerSettings = KafkaUtils.configureProducerSettings()
+
+  val producerConfig = ConfigFactory.load.getConfig("akka.kafka.producer")
+  val producerSettings = ProducerSettings(producerConfig, new StringSerializer, new ByteArraySerializer)
+    .withBootstrapServers(Configuration.bootstrapServer)
+
   val committerSettings = CommitterSettings(system).withMaxBatch(1L).withParallelism(1)
 
   Consumer
     .committableSource(consumerSettings, Subscriptions.topics(Configuration.ingestEnrichTopic))
     .map { msg =>
+      val byteArray = serialize(msg.record.value())
       ProducerMessage.multi(
-        List[ProducerRecord[String, String]](
-          new ProducerRecord[String, String](Configuration.enrichEsprocTopic, jsonToAvro(msg.record.value)),
-          new ProducerRecord[String, String](Configuration.enrichCassTopic, jsonToAvro(msg.record.value))
+        List[ProducerRecord[String, Array[Byte]]](
+          new ProducerRecord[String, Array[Byte]](Configuration.enrichEsprocTopic, byteArray),
+          new ProducerRecord[String, Array[Byte]](Configuration.enrichCassTopic, byteArray)
         ),
         msg.committableOffset
       )
@@ -32,8 +45,29 @@ object EnrichmentProc extends App with DefaultJsonProtocol {
     .toMat(Producer.committableSink(producerSettings, committerSettings))(Keep.both)
     .run()
 
-  def jsonToAvro(msg: String): String = {
-    val converter = new JsonToAvroConverter("me.skatz.kafka")
-    converter.convert("test", msg).toString(true)
+  def serialize(msg: String): Array[Byte] = {
+    val gson = new Gson
+
+    // Deserialize JSON
+    val message = gson.fromJson(msg, classOf[TweeterMessage])
+
+    val schema = AvroSchema[TweeterMessage]
+    val genericRecord: GenericRecord = new GenericData.Record(schema)
+
+    genericRecord.put("firstName", message.firstName)
+    genericRecord.put("lastName", message.lastName)
+    genericRecord.put("tweet", message.tweet)
+    genericRecord.put("date", message.date)
+
+    // Serialize generic record into byte array
+    val writer = new GenericDatumWriter[GenericRecord](schema)
+    val out = new ByteArrayOutputStream()
+    val encoder: BinaryEncoder = EncoderFactory.get().binaryEncoder(out, null)
+    writer.write(genericRecord, encoder)
+    encoder.flush()
+    out.close()
+    out.close()
+
+    out.toByteArray
   }
 }
